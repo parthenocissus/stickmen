3590b68a6a2d8b6e13ee178e5e13baa695e548f3
parthenocissus <!smokve21>
2023-07-21 12:37:46 +0200

    testing...

--- bin/axidraw_test4.py
+++ bin/axidraw_test4.py
@@ -1,7 +1,6 @@
 from pyaxidraw import axidraw
 from svgpathtools import svg2paths
 import time
-import threading
 
 # svg_file = "static/media/final_svgs/arrows_002_random_order_test2.svg"
 svg_file = "static/media/final_svgs/arrowtest2.svg"
@@ -12,34 +11,41 @@ if not ad.connect():
     quit()
 
 # ad.plot_setup(svg_file)
-paths, attributes = svg2paths(svg_file)
 # ad.options.units = 1
-# ad.update()
-ad.moveto(0, 0) # refresh pos u slucaju da je prosli run chrashovao u sred crtanja
-# ad.moveto(1, 1)
+ad.update()
+paths, attributes = svg2paths(svg_file)
+ad.moveto(1, 1)
 
 threads = []
+points = []
 
 def draw_and_wait(path):
-    ad.line(path[0].start.real, path[0].start.real)
+    # ad.line(path[0].start.real, path[0].start.real)
+    points.append({
+        "x": path[0].start.real,
+        "y": path[0].start.imag
+    })
     # ad.pendown()
-    for point in paths:
-        ad.line(point.end.real, point.end.imag)
-    ad.penup()
-    time.sleep(2)
-    i = 0
-    i=+1
-    print("function completed " + i )
+    for seg in path:
+        # ad.line(seg.end.real, seg.end.imag)
+        points.append({
+            "x": seg.end.real,
+            "y": seg.end.imag
+        })
+    # ad.penup()
+    # time.sleep(3)
 
 
 for i, j in enumerate(paths):
-    for path in paths:
-        thread = threading.Thread(target=draw_and_wait, args=(path,))
-        thread.start()
-        threads.append(thread)
 
-for t in threads:
-    t.join()
+    for path in paths:
+        draw_and_wait(path)
+
+
+for p in points:
+    ad.line(p["x"], p["y"])
+    ad.penup()
+    time.sleep(2)
 
 
 svg_final = draw_and_wait(paths)
