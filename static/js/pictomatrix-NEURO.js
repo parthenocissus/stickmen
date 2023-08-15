30cc2e4d3bc887392e301204a084e95670a4f797
2alf <113948114+2alf@users.noreply.github.com>
2023-08-15 20:12:12 +0200

    testing...

--- static/js/pictomatrix-NEURO.js
+++ static/js/pictomatrix-NEURO.js
@@ -1,42 +1,48 @@
-const startOfPictos = 0;
-const endOfPictos = 20;
+const startOfPictos = 1;
+const endOfPictos = 100;
 
 let nOfPictos;
 let showPoints = false;
 let pictoScaleFactor = 3;
-let scaleFactor = 30;
+let scaleFactor = 40;
 let size;
 let drawings;
 let pictograms;
 
-const unit = { x: 35, y: 50 };
-// const unit = { x: 17, y: 27 };
-const strokeData = { base: 2, bold: 2 };
-const margins = { x: 50, y: 20 };
-const spacing = 2;
+const unit = { x: 17, y: 35 }; //x: 17, y: 35
+const gap = 12; // bacgkround gap
+const dim = { w: 40, h: 27 }; //w: 40, h: 27
+// const strokeData = { base: 1, bold: 4 };
+const margins = { x: 50, y: 50 };
+const spacing = 2; // real gap
 
-const localhost = "http://127.0.0.1:5000/static/media/rnntestOut/";
+const localhost = "http://127.0.0.1:5000/static/media/GlyphsTest/";
 
 function preload() {
+    unit.x = unit.x + gap;
+    unit.y = unit.y + gap;
     nOfPictos = endOfPictos - startOfPictos + 1;
     pictograms = new Array(nOfPictos);
 
     for (let i = startOfPictos; i <= endOfPictos; i++) {
-        const svgUrl = localhost + i + ".svg";
+        var svgUrl = localhost + i + ".svg";
         pictograms[i - startOfPictos] = loadImage(svgUrl);
     }
 }
 
 function setup() {
     drawings = shuffle(pictograms);
-    viewSize = {x: unit.x * scaleFactor + 2 * margins.x, y: unit.y * scaleFactor + 2 * margins.y};
-    // createCanvas(viewSize.x, viewSize.y, SVG);
-    createCanvas(viewSize.x/2, viewSize.y/2, SVG)
+    viewSize = {
+        x: (unit.x * pictoScaleFactor * dim.w) + (2 * margins.x),
+        y: (unit.y * pictoScaleFactor * dim.h) + (2 * margins.y)
+    };
+    path = querySVG('path')
+    createCanvas(viewSize.x, viewSize.y, SVG);
 }
+
 
 function draw() {
     background(230);
-
     const availableWidth = width - 2 * margins.x;
     const numColumns = Math.floor(availableWidth / (unit.x * pictoScaleFactor + spacing));
     const cellWidth = (availableWidth - (numColumns - 1) * spacing) / numColumns;
@@ -52,6 +58,7 @@ function draw() {
             let img = drawings[counter++];
             let shift = {x: i, y: j};
             image(img, shift.x, shift.y, cellWidth, unit.y * pictoScaleFactor);
+            // path.attribute('stroke-width', 2);
         }
     }
 }
